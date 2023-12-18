baa312a313d81c2ff6fb205167e7da1df6926068
Astro <136151272+AstroVentures@users.noreply.github.com>
2023-12-18 17:51:32 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [],
   server: {
     host: '0.0.0.0',
-    hmr: false, // Change this line to `false` disable auto-refreshing.
+    hmr: false, // Change this line to false disable auto-refreshing.
   }
 })
